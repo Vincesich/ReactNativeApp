dcd08b1a951f8a0182907079a07c7d508913cd7b
Vince <vince.sich@gmail.com>
2024-02-15 20:16:42 +0300

    added use of react hook form in the help page

--- screens/HelpScreen.tsx
+++ screens/HelpScreen.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+
+interface FormData {
+  comment: string;
+}
 
 const HelpScreen: React.FC = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const [submissionResult, setSubmissionResult] = useState<string | null>(null);
+
+  const onSubmit = async (data: FormData) => {
+    console.log(data.comment); 
+    setSubmissionResult('Thank you for your feedback!');
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>For any queries and support you can reach me on email at vince.sich@gmail.com</Text>
+      <Text>Feel free to leave a comment:</Text>
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Your Comment"
+            onChangeText={onChange}
+            value={value}
+            onBlur={onBlur}
+            keyboardType="default"
+            multiline={true}
+            numberOfLines={5}
+            style={{ borderWidth: 1, padding: 10, marginVertical: 5 }}
+          />
+        )}
+        name="comment"
+        rules={{ required: 'Comment is required' }}
+      />
+      {errors.comment && <Text style={{ color: 'red' }}>{errors.comment.message}</Text>}
+
+      <Button title="Submit Feedback" onPress={handleSubmit(onSubmit)} />
+
+      {submissionResult && <Text style={{ padding: 10 }}>{submissionResult}</Text>}
     </View>
   );
 };
--- store/userApi.ts
+++ store/userApi.ts
@@ -8,3 +8,4 @@ export const fetchUserById = async (userId: string) => {
     return response.json();
   };
   
+  
