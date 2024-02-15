6e8c8a2d44432460920c4b3b04fe8ecb75fda644
Vince <vince.sich@gmail.com>
2024-02-15 18:14:10 +0300

    added fullstop

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -5,7 +5,7 @@ const AboutScreen: React.FC = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Hello, Im Vince</Text>
-      <Text>I built this app to demonstrate my competence in react native</Text>
+      <Text>I built this app to demonstrate my competence in react native.</Text>
     </View>
   );
 };
